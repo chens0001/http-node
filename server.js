332c4fc118a11ce2bd5f5d783699c5b27b2490ab
mac <mac@macdeMacBook-Air.local>
2022-04-06 22:47:06 +0800

    修复完成

--- server.js
+++ server.js
@@ -24,17 +24,18 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`<h1>hello 啊</h1>`)
+    response.write(`
+      <link rel="stylesheet" href="/style">
+      <h1>你好</h1>
+    `)
     response.end()
-  } else if(path === '/css'){
+  } else if(path === '/style'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`h1{color: red;}`)
     response.end()
   } else {
     response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
     response.end()
   }
 
